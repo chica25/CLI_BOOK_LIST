82a32325436b31ab743bfe7de4a11df8f4a35e38
chica25 <pfco520@gmail.com>
2022-12-21 08:34:17 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in cli_book_list.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/cli_book_list.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cli_book_list/version"
+
+module CliBookList
+  class Error < StandardError; end
+  # Your code goes here...
+end
